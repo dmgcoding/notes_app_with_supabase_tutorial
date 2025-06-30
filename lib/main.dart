5391186f37b1a96192cbbaa6788512145e024f43
Shehan Niwanthaka <dmgcoding@gmail.com>
2025-06-30 12:39:21 +0530

    feat: ui done

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,9 @@ import 'package:notes_with_supabase/pages/auth.dart';
 import 'package:notes_with_supabase/pages/notes.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-//get these from envs in production
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  //make sure you read the readme.md about how to run the app
   const supabaseUrl = String.fromEnvironment('SUPABASE_URL');
   const supabaseKey = String.fromEnvironment('SUPABASE_KEY');
   final supabase = await Supabase.initialize(
